3621e8d842eb00b4d4ec46645eab87845c8cf22a
gimme <gimmeitorilltell@gmail.com>
2018-06-05 02:23:34 -0400

    berkeley: Candify, add Deps & update extract files
    Change-Id: Ia8ebc5cbe7a2f109b315535e9e0a8f04ee3466ff

--- extract-files.sh
+++ extract-files.sh
@@ -24,9 +24,9 @@ VENDOR=huawei
 MY_DIR="${BASH_SOURCE%/*}"
 if [[ ! -d "$MY_DIR" ]]; then MY_DIR="$PWD"; fi
 
-LINEAGE_ROOT="$MY_DIR"/../../..
+CANDY_ROOT="$MY_DIR"/../../..
 
-HELPER="$LINEAGE_ROOT"/vendor/lineage/build/tools/extract_utils.sh
+HELPER="$CANDY_ROOT"/vendor/candy/build/tools/extract_utils.sh
 if [ ! -f "$HELPER" ]; then
     echo "Unable to find helper script at $HELPER"
     exit 1
@@ -55,7 +55,7 @@ if [ -z "$SRC" ]; then
 fi
 
 # Initialize the helper
-setup_vendor "$DEVICE" "$VENDOR" "$LINEAGE_ROOT"
+setup_vendor "$DEVICE" "$VENDOR" "$CANDY_ROOT"
 
 extract "$MY_DIR"/proprietary-files.txt "$SRC"
 
